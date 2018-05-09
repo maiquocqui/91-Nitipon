78d3a8db1f600b0d9941cfd29a3030f9153dcd63
maiquocqui <maiquocqui@gmail.com>
2018-05-09 14:41:32 +0700

    Initial commit

---
+++ dist/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+$(document).ready(function () {
+    // BANNER
+    $('.banner').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: true,
+        dots: true,
+        fade: true
+    });
+});
